487a277847ec97ab984c23f4e0741000e3df826c
vincedl-dev <vincelavador.dev@gmail.com>
2022-03-02 17:26:10 +0800

    added new elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return (
         <div>
-            <p>This is the Home page Components</p>
-            <p>sample components</p>
+            <div>dsadsadasdsad</div>
+      
         </div>
     );
 }
